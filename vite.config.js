965b0f4465beda217cbd2d3cd61d14612fc12bf0
ShardulAswale <unrealdoc12@gamil.com>
2024-06-12 11:42:58 +0530

    fix : 404

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  plugins: [react()],
   base: '/js-notebook/',  // base path should match your repository name
   // other configurations...
 });
